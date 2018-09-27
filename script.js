840d614c9d133b4f7119cd3976c623a4d060e3a9
Aelto <hottou.thibault@gmail.com>
2018-09-27 21:06:20 +0200

    add post fetching

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+const app = document.querySelector('.app')
 
 function get_meta() {
   return fetch('posts/meta.json')
@@ -8,10 +8,56 @@ function get_meta() {
 
 async function main() {
   const meta = await get_meta()
+  const { posts } = meta
+  
+  if (!posts.length) {
+    return app.innerHTML = `<h1>No posts found :(</h1>`
+  }
 
-  console.log(meta)
+  let current_post_index = 0
 
-  document.write(meta)
+  const next_button = document.querySelector('button.next')
+  next_button.addEventListener('click', e => {
+    const { target } = e
+
+    if (current_post_index === posts.length - 1) {
+      return
+    }
+
+    current_post_index = read_post_at(posts, current_post_index + 1)
+
+    if (current_post_index === posts.length - 1) {
+      target.classList.add('limit')
+    }
+  })
+
+  const previous_button = document.querySelector('button.previous')
+  previous_button.addEventListener('click', e => {
+    const { target } = e
+
+    if (!current_post_index) {
+      return
+    }
+
+    current_post_index = read_post_at(posts, current_post_index - 1)
+
+    if (!current_post_index) {
+      target.classList.add('limit')
+    }
+  })
+}
+
+async function read_post_at(posts, index) {
+  const post_file = posts[index]
+  const post_markdown = await fetch(`posts/${post_file}`)
+    .then(r => r.text())
+
+  const converter = new showdown.Converter()
+  const html = converter.makeHtml(post_markdown)
+
+  app.innerHTML = html
+
+  return index
 }
 
 requestAnimationFrame(main)
